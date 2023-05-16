c9e8c55e28a5b1401ac302a99b489c49c997be8f
Thiago-S-Sousa <128821560+Thiago-S-Sousa@users.noreply.github.com>
2023-05-16 18:58:02 -0300

    Adicionando a diretiva Activatedroute ao photo service e atualizando chamada http

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { PhotoService } from '../photo/photo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-list',
@@ -10,11 +11,16 @@ import { PhotoService } from '../photo/photo.service';
 export class PhotoListComponent implements OnInit {
   photos: any[] = [];
 
-  constructor(private photoService: PhotoService) {}
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute // Ativa rotas aleatorias passadas no momento
+  ) {}
 
   ngOnInit(): void {
+    const userName = this.activatedRoute.snapshot.params['userName'];
+
     this.photoService
-      .listFromUser('flavio')
+      .listFromUser(userName)
       .subscribe((photos) => (this.photos = photos));
   }
 }
