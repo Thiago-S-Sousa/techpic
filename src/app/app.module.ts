e93db7423762649ee7c71d090018e620a1c17981
Thiago-S-Sousa <128821560+Thiago-S-Sousa@users.noreply.github.com>
2023-05-16 18:54:48 -0300

    Adicionando errors module ao app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorsModule } from './errors/errors.module';
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PhotosModule],
+  imports: [BrowserModule, AppRoutingModule, PhotosModule, ErrorsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
