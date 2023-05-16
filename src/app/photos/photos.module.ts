a4289cfd74cb239e186c36ec722e132e458fd0d8
Thiago-S-Sousa <128821560+Thiago-S-Sousa@users.noreply.github.com>
2023-05-16 10:42:02 -0300

    Atualizando módulo de photos

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-  declarations: [PhotoComponent],
-  exports: [PhotoComponent], //exports: deixa o modulo publico
-  imports: [HttpClientModule],
+  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent],
+  exports: [], //exports: deixa o modulo publico
+  imports: [HttpClientModule, CommonModule], //CommomModule - è uma boa prática já importar a cada módulo novo
 })
 export class PhotosModule {}
