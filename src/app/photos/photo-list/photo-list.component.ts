3bbe5e3bdce1ecd01e60043b66502943321c6fa5
Thiago-S-Sousa <128821560+Thiago-S-Sousa@users.noreply.github.com>
2023-05-17 14:39:52 -0300

    Implementando padrão debounce com RxJS e ngOnDestroy

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 
@@ -8,12 +10,13 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css'],
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   onKeyup(event: any) {
-    this.filter = event.target.value;
+    this.debounce.next(event.target.value);
   }
   constructor(
     private activatedRoute: ActivatedRoute // Ativa rotas aleatorias passadas no momento
@@ -21,5 +24,13 @@ export class PhotoListComponent implements OnInit {
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos'];
+
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe((filter) => (this.filter = filter));
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 }
